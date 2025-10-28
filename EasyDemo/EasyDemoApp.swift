a5630187492c2e9609319725c4e0aa8095279acc
Daniel Oquelis <daniel.oquelis@gmail.com>
2025-10-28 02:09:53 +0100

    Initial Commit

---
+++ EasyDemo/EasyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyDemoApp.swift
+//  EasyDemo
+//
+//  Created by Daniel Oquelis on 28.10.25.
+//
+
+import SwiftUI
+
+@main
+struct EasyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EasyDemoTests/EasyDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  EasyDemoTests.swift
+//  EasyDemoTests
+//
+//  Created by Daniel Oquelis on 28.10.25.
+//
+
+import Testing
+@testable import EasyDemo
+
+struct EasyDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
